7e721565164b53ea71d1bd431d7e3bf37460deda
komat0 <111368527+komat0@users.noreply.github.com>
2022-08-30 16:05:14 +0300

    Done!

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Cat {
+public class Cat {
+    private final String name;
+    private int hunger;
+
+    public Cat(String name, int hunger) {
+        this.name = name;
+        this.hunger = hunger;
+    }
+
+    public int getHunger() {
+        System.out.println(name + " want to eat " + hunger + " gram of food");
+        return hunger;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public void eat(Plate plateName) {
+        if (hunger <= plateName.getFullness()) {
+            plateName.removeFullness(hunger);
+            setHunger(0);
+            System.out.println(name + " ate and now didn't hungry");
+        } else {
+            System.out.println("There is not enough food in this plate to fully feed the " + name + ". Try another plate.");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Plate bluePlate = new Plate("Blue Plate", 45);
+        Cat[] cats = new Cat[4];
+        cats[0] = new Cat("Barsik", 25);
+        cats[1] = new Cat("Red", 5);
+        cats[2] = new Cat("Vaska", 20);
+        cats[3] = new Cat("Faraon", 15);
+
+        bluePlate.getPlateInfo();
+        for (Cat cat : cats) {
+            cat.eat(bluePlate);
+        }
+        bluePlate.getPlateInfo();
+        bluePlate.addFullness(100);
+        bluePlate.getPlateInfo();
+
     }
 }
