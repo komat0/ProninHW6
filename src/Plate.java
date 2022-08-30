089e00da4bf3618d7317854cf48771e1b1f26d18
komat0 <111368527+komat0@users.noreply.github.com>
2022-08-30 16:34:43 +0300

    Done!

--- src/Plate.java
+++ src/Plate.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Plate {
+public class Plate {
+    private final String name;
+    private int fullness;
+
+    public Plate(String name, int fullness) {
+        this.name = name;
+        this.fullness = fullness;
+    }
+
+    public int getFullness() {
+        return fullness;
+    }
+
+    public void addFullness(int addFood) {
+        int newFood = fullness + addFood;
+        fullness = newFood;
+    }
+
+    public void removeFullness(int removeFood) {
+        int newFood = fullness - removeFood;
+        fullness = newFood;
+    }
+
+    public void getPlateInfo() {
+        System.out.println("There is a " + fullness + " gram in the " + name);
+    }
 }
